0e84c53603aa14def3ff31af587f678ae89fe6de
fewtha <few_tha@kkumail.com>
2022-01-20 00:36:46 +0700

    1

---
+++ app/src/main/java/com/example/custom_listview/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.custom_listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+
+
+import com.example.custom_listview.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        int[] imageId = {R.drawable.a,R.drawable.b,R.drawable.c};
+        String[] name = {"Thanawat kooponthong","Aueaungkul sroyudom","Thanakon Piromchai"};
+        String[] lastMessage = {"Smart","Fin","Fuse"};
+        String[] lastmsgTime = {"8:45 pm","9:00 am","7:34 pm"};
+        String[] phoneNo = {"0981542279","0869415512","08258321271"};
+        String[] country = {"Nongkhai","Chaiyaphum","Chaiyaphum"};
+
+        ArrayList<User> userArrayList = new ArrayList<>();
+
+        for(int i = 0;i< imageId.length;i++){
+
+            User user = new User(name[i],lastMessage[i],lastmsgTime[i],phoneNo[i],country[i],imageId[i]);
+            userArrayList.add(user);
+
+        }
+
+
+        ListAdapter listAdapter = new ListAdapter(MainActivity.this,userArrayList);
+
+        binding.listview.setAdapter(listAdapter);
+        binding.listview.setClickable(true);
+        binding.listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent i = new Intent(MainActivity.this,UserActivity.class);
+                i.putExtra("name",name[position]);
+                i.putExtra("phone",phoneNo[position]);
+                i.putExtra("country",country[position]);
+                i.putExtra("imageid",imageId[position]);
+                startActivity(i);
+
+            }
+        });
+
+    }
+}
